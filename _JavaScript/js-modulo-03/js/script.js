3c6bbf9c826af55240154f8d6a9d740b682ea5a0
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-06 18:42:16 -0300

    finished simple js with for loop

--- _JavaScript/js-modulo-03/js/script.js
+++ _JavaScript/js-modulo-03/js/script.js
@@ -1,1 +1,28 @@
-console.log("Olá mundo!");
+
+let input = document.querySelector("input");
+let btnSearch = document.querySelector(".btn-grad");
+
+let showTextReturn = document.getElementById("text__return");
+let showSearchReturn = document.getElementById("search__return");
+
+let contacts = [
+    { name: 'Marcos', number: '(11)-92337-5944' },
+    { name: 'Luiza', number: '(11)-97233-4567' },
+    { name: 'Lula', number: '(11)-00171-0171' },
+    { name: 'Bozo', number: '(11)-00171-1171' },
+]
+
+function searchList() {
+
+    for (let i = 0; i < contacts.length; i++) {
+
+        if (input.value.toLowerCase() === contacts[i].name.toLowerCase()) {
+            showTextReturn.innerHTML = `Contato encontrado`;
+            showSearchReturn.innerHTML = `Nome: ${contacts[i].name} </br>
+            Tel: ${contacts[i].number}`;
+            break;
+        } else {
+            showTextReturn.innerHTML = `Contato não encontrado`;
+        }
+    }
+}
