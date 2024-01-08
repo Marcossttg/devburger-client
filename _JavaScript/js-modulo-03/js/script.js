8c73e24ae564fb2599739fece610b857fc1e15d8
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-08 20:09:07 -0300

    test with for while

--- _JavaScript/js-modulo-03/js/script.js
+++ _JavaScript/js-modulo-03/js/script.js
@@ -12,20 +12,40 @@ let contacts = [
     { name: 'Bozo', number: '(11)-00171-1171' },
 ]
 
-// com for in 
+// com while 
 function searchList() {
-    for (const key in contacts) {
 
-        if (input.value.toLowerCase() === contacts[key].name.toLowerCase()) {
+    let i = 0;
+    while (i < contacts.length) {
+
+        if (input.value.toLowerCase() === contacts[i].name.toLowerCase()) {
+            console.log(contacts[i])
             showTextReturn.innerHTML = `Contato encontrado`;
-            showSearchReturn.innerHTML = `Nome: ${contacts[key].name}</br>
-            Tel: ${contacts[key].number}`;
+            showSearchReturn.innerHTML = `Nome: ${contacts[i].name} </br>
+            Tel: ${contacts[i].number}`;
             break;
         } else {
             showTextReturn.innerHTML = `Contato não encontrado`;
         }
+        i++
     }
 }
+
+
+// com for in
+// function searchList() {
+//     for (const key in contacts) {
+
+//         if (input.value.toLowerCase() === contacts[key].name.toLowerCase()) {
+//             showTextReturn.innerHTML = `Contato encontrado`;
+//             showSearchReturn.innerHTML = `Nome: ${contacts[key].name}</br>
+//             Tel: ${contacts[key].number}`;
+//             break;
+//         } else {
+//             showTextReturn.innerHTML = `Contato não encontrado`;
+//         }
+//     }
+// }
 
 // com for normal
 // function searchList() {
