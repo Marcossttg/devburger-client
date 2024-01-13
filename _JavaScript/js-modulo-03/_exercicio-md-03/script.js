d1a96cfb3dcba3b53c94732835b2d3e72e78bf6e
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-12 23:45:44 -0300

    reation of defer-js and variables

--- _JavaScript/js-modulo-03/_exercicio-md-03/script.js
+++ _JavaScript/js-modulo-03/_exercicio-md-03/script.js
@@ -1,1 +1,40 @@
-console.log('Olá Mundo.')
+
+let n1 = document.querySelector("#input-n1");
+let n2 = document.querySelector("#input-n2");
+
+//botoes
+let btnSomar = document.querySelector("#somar");
+let btnSubtrair = document.querySelector("#subtrair");
+let btnResultado = document.querySelector("#resultado");
+
+//text resultado
+let textResult = document.querySelector(".texto-resultado");
+let paragraphResult = document.querySelector("#resultado-numero");
+
+
+function somar() {
+    //Verifica campo vazio
+    // if (n1.value === "") {
+    //     textResult.innerHTML = `Primeiro campo está vazio, digite um número.`
+    // }
+
+    // if (n2.value === "") {
+    //     textResult.innerHTML = `Segundo campo está vazio digite um número`
+    // }
+
+    let resultSoma = (parseInt(n1.value) + parseInt(n2.value));
+    console.log(resultSoma)
+    return resultSoma
+
+}
+
+function subtrair() {
+    let resultSub = (parseInt(n1.value) - parseInt(n2.value));
+    console.log(resultSub)
+    return resultSub
+}
+
+function resultado() {
+    textResult.innerHTML = `O resultado é:`
+    paragraphResult.textContent = `${somar()}`
+}
