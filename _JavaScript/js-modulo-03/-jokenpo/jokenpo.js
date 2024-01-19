b96411a941d6deaadebde84c52891f4a055a2faf
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-18 21:03:27 -0300

    adding ENUM method

--- _JavaScript/js-modulo-03/-jokenpo/jokenpo.js
+++ _JavaScript/js-modulo-03/-jokenpo/jokenpo.js
@@ -8,6 +8,16 @@ let opponentPoints = document.querySelector("#points__player02");
 let youPlayerNumber = 0;
 let opponentPointsNumber = 0;
 
+/* Utilização de ENUM no javascript
+Enum é um tipo de dados especial que permite definir um conjunto de valore nomeado, no javascript. Ele e um Objeto utilizado para definir um conjunto de valores nomeados, epeciais. Utilização de ENUM, permite Legibilidade do Código,
+Manutenção simplificada, Evitar erros de digitação e ect.*/
+//ENUM  
+const ITENS_THE_GAME = {
+    ROCK: "rock",
+    PAPER: "paper",
+    SCISSORS: "scissor"
+}
+
 //pegar o click
 const startGame = (yourChoice) => {
     startTheGame(yourChoice, secondPlayer())
@@ -16,7 +26,7 @@ const startGame = (yourChoice) => {
 //segundo jogador 
 const secondPlayer = () => {
 
-    let choices = ["rock", "paper", "scissors"]; //array com valores 
+    let choices = [ITENS_THE_GAME.ROCK, ITENS_THE_GAME.PAPER, ITENS_THE_GAME.SCISSORS]; //array com valores 
     let sortChoices = Math.floor(Math.random() * 3); //sortear valor
 
     return choices[sortChoices]
@@ -30,9 +40,9 @@ const startTheGame = (youPlayer, machinePlayer) => {
         h3.innerHTML = `Deu empate!`;
 
     } else if (//qnd eu ganhar
-        (youPlayer === 'paper' && machinePlayer === 'rock') ||
-        (youPlayer === 'rock' && machinePlayer === 'scissors') ||
-        (youPlayer === 'scissors' && machinePlayer === 'paper')) {
+        (youPlayer === ITENS_THE_GAME.PAPER && machinePlayer === ITENS_THE_GAME.ROCK) ||
+        (youPlayer === ITENS_THE_GAME.ROCK && machinePlayer === ITENS_THE_GAME.SCISSORS) ||
+        (youPlayer === ITENS_THE_GAME.SCISSORS && machinePlayer === ITENS_THE_GAME.PAPER)) {
         youPlayerNumber++; //conta pontos
         youPoints.innerHTML = youPlayerNumber; //mostra ponto
         h3.innerHTML = `Você ganhou!`;
