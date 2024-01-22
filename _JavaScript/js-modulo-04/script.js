1450b8c8ce474254a0c602326edb9682def5d1e4
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-21 22:26:04 -0300

    finished

--- _JavaScript/js-modulo-04/script.js
+++ _JavaScript/js-modulo-04/script.js
@@ -1,6 +1,16 @@
 const btnForEach = document.getElementById('mtForeach');
 const list = document.querySelector("ul");
-let listItens = '';
+
+/* formata para moeda, para tipo de moeda para trocar o tipo basta
+troca o pais de origem, e depois currency para o valor local
+ */
+function formatCurrency(value) {
+    const valueBrl = value.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    return valueBrl
+}
 
 function btnShowAll(newProductsArray) {
     listItens = '';
@@ -10,7 +20,7 @@ function btnShowAll(newProductsArray) {
         <li>
         <img src=${product.src}>
         <p>${product.name} </p>
-        <p class="valor__produto">${product.price.toFixed(2)}</p>
+        <p class="valor__produto">${formatCurrency(product.price)}</p>
         </li>
         `
     });
@@ -42,18 +52,25 @@ btnMap.addEventListener('click', () => {
 });
 
 //Reduce
-
 const btnReduceAll = document.getElementById("mtReduce");
 
 function sumValueAll() {
     const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
     list.innerHTML = `
     <li>
-   
     <p>O Valor total dos produtos é:</p>
-    <p class="valor__produto">R$ ${totalValue.toFixed(2)}</p>
+    <p class="valor__produto">${formatCurrency(totalValue)}</p>
     </li>
     `;
 }
 
 btnReduceAll.addEventListener("click", sumValueAll);
+
+//filter 
+const btnFilter = document.getElementById("mtFilter");
+
+function filterAll() {
+    const resultFilter = menuOptions.filter((product) => product.vegan)
+    btnShowAll(resultFilter);
+}
+btnFilter.addEventListener("click", filterAll);
