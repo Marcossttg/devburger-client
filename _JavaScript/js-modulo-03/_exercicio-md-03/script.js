656e4a4fff6e83cd9db7099063e14076e56ffab5
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-01-30 23:48:03 -0300

    doing exercise

--- _JavaScript/js-modulo-03/_exercicio-md-03/script.js
+++ _JavaScript/js-modulo-03/_exercicio-md-03/script.js
@@ -1,3 +1,22 @@
+
+//Exercícios Md-02
+let btn = document.querySelector(".container button");
+let h2 = document.querySelector("h2");
+
+btn.addEventListener('click', () => {
+    let input = document.querySelector("input").value;
+    document.querySelector("input").focus();
+    let n = input % 2;
+
+    if (n == 0) {
+        console.log('e par ' + n)
+        h2.textContent = `Par`;
+    } else {
+        console.log("e inpar " + n)
+        h2.textContent = `Ímpar`;
+    }
+});
+
 /* 
 let n1 = document.querySelector("#input-n1");
 let n2 = document.querySelector("#input-n2");
@@ -33,6 +52,8 @@ function subtrair() {
 }
  */
 
+
+/* 
 //Calculadora 
 
 function addCaracter(caracter) {
@@ -54,4 +75,4 @@ function deleteLast() {
 function calculate() {
     const valorDisplay = document.querySelector("input").value;
     document.querySelector("input").value = eval(valorDisplay)
-}
+} */
