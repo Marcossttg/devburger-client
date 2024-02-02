c32e66b5098752444818906b45d379376f587cf4
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-02-02 19:18:16 -0300

    starting exercises from module 01 and web

---
+++ _JavaScript/js-modulo-01/Exercicios-01/script.js
@@ -0,0 +1,17 @@
+alert("Olá Mundo!");
+
+/* ## Módulo 1
+
+1. Escreva um programa que exiba "Olá, mundo!" no console.
+2. Escreva um programa que calcule a média de três números e exiba o resultado no console.
+    - Para calcular a média você só precisa somar os 3 números e dividir por 3.
+3. Escreva um programa que verifique se um número é maior que 10 e exiba o resultado no console.
+4. Escreva um programa que verifique se um número é positivo e exiba o resultado no console.
+5. Escreva um programa que calcule a soma de dois números e exiba o resultado utilizando uma template string. Por exemplo, se os números forem 5 e 3, o programa deve exibir "A soma de 5 e 3 é igual a 8".
+
+# Módulo Web
+
+1. Crie uma página HTML com um botão e um parágrafo. Escreva um programa que, ao clicar no botão, altere o texto do parágrafo para "Olá, mundo!" utilizando o método getElementById.
+2. Crie uma página HTML com vários elementos de âncora (<>). Escreva um programa que, ao clicar em um link, exiba um alerta com o texto do link utilizando o método getElementByTagName.
+3. Crie uma página HTML com um botão e um parágrafo. Escreva um programa que, ao clicar no botão, altere o texto do parágrafo para "Olá, mundo!" utilizando o método **querySelector**.
+4. Crie uma página HTML com várias divs com a classe "item". Escreva um programa que altere o texto de todas as divs para "Item alterado" utilizando o método **querySelectorAll**. */
