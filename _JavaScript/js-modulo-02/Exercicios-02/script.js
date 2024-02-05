4a4f436da09a6db9bb57a1c0f7856c8b0e8bb8ae
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-02-04 22:08:45 -0300

    finished module 2

--- _JavaScript/js-modulo-02/Exercicios-02/script.js
+++ _JavaScript/js-modulo-02/Exercicios-02/script.js
@@ -1,7 +1,5 @@
-
-
 //# Exercícios Módulo 2
-/*
+
 //1. Escreva um programa que verifique se um número é par ou ímpar e exiba o resultado no console.
 
 function createElement() {//Criando uma função que vai criar um botão, input, texto.
@@ -22,10 +20,10 @@ function createElement() {//Criando uma função que vai criar um botão, input,
         //depois de calcular se o valor digitado e resto 0
 
         if (n == 0) {//if Comparando se o valor do n e igual a 0
-            console.log('e par ' + n)
+            console.log('e par ' + n);
             h2.textContent = `Par`;
         } else {
-            console.log("e inpar " + n)
+            console.log("e inpar " + n);
             h2.textContent = `Ímpar`;
         }
     });
@@ -33,11 +31,11 @@ function createElement() {//Criando uma função que vai criar um botão, input,
     document.body.appendChild(btn);//Aqui estamos adicionando o botão no body do HTML
 }
 createElement()//Chamado a função que vai criar um input e um botão;
-*/
+/* */
 
 //2. Escreva um programa que calcule o resto da divisão entre dois números inteiros
 //e exiba o resultado.
-/* 
+/*
 function createElement() {
     let btn = document.createElement("button");
     let textBtn = document.createTextNode("Clique");
@@ -74,11 +72,58 @@ function createElement() {
 }
 createElement()
 */
-//3. Escreva um programa que calcule a média de três números reais e exiba o resultado.
-let btn = document.createElement("button")
-console.log(btn)
 
 /*
-4. Escreva um programa que compare duas strings e exiba "As strings são iguais" se as strings forem iguais, ou "As strings são diferentes" se forem diferentes.
-5. Escreva um programa que compare a idade de duas pessoas e exiba "A primeira pessoa é mais velha" se a idade da primeira pessoa for maior que a idade da segunda pessoa, ou "A segunda pessoa é mais velha" se for menor. 
+//3. Escreva um programa que calcule a média de três números reais e exiba o resultado.
+
+let btn = document.querySelector("button");
+let input = document.querySelectorAll("input");
+
+document.body.querySelector("h2").textContent = "Digite três números:"
+
+btn.onclick = function () {
+    let media = 0;
+    for (let i = 0; i < input.length; i++) {
+        let somaMedia = Number(document.getElementById("input" + i).value);
+        media += somaMedia;
+    }
+    document.body.querySelector("h2").textContent = `A Média é: ${(media / 3).toFixed(1)}`
+}
+*/
+// 4. Escreva um programa que compare duas strings e exiba "As strings são iguais" se as strings forem
+// iguais, ou "As strings são diferentes" se forem diferentes.
+
+/* 
+let inputText01 = document.getElementById("input0");
+let inputText02 = document.getElementById("input1");
+let p = document.querySelector("p");
+
+document.getElementById("input2").style.display = "none";
+
+document.querySelector("button").onclick = () => {
+    
+    if (inputText01.value == inputText02.value) {
+        p.textContent = "As strings são iguais"
+    } else {
+        p.textContent = "As strings são diferentes"
+    }
+} 
+*/
+
+/*
+
+// 5. Escreva um programa que compare a idade de duas pessoas e exiba "A primeira pessoa é mais velha" se a idade da primeira pessoa for maior que a idade da segunda pessoa, ou "A segunda pessoa é mais velha" se for menor. 
+const input = document.getElementById("input0");
+const input1 = document.getElementById("input1");
+document.getElementById("input2").style.display = "none";
+
+document.querySelector("button").onclick = () => {
+    if (input.value == input1.value) {
+        document.querySelector("p").textContent = "A idade são iguais";
+    } else if (input.value > input1.value) {
+        document.querySelector("p").textContent = "A primeira pessoa é mais velha";
+    } else {
+        document.querySelector("p").textContent = "A segunda pessoa é mais velha";
+    }
+}
 */
