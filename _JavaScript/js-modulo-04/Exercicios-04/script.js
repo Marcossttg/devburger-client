c199ce19e5cf8caf18ccab39ff7a782e5fc620ef
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-02-06 22:15:05 -0300

    completed exercise 1

---
+++ _JavaScript/js-modulo-04/Exercicios-04/script.js
@@ -0,0 +1,21 @@
+
+//# Módulo 4
+//1. Escreva um programa que receba uma lista de números e retorne uma nova lista com o dobro de cada número.
+/* 
+const n = [10, 20, 30, 40, 50];
+const n2 = n.map(function (num) {
+    return num * 2;
+})
+console.log(n);
+console.log(n2); 
+*/
+
+2. Escreva um programa que receba uma lista de nomes e retorne uma nova lista com o comprimento de cada nome.
+
+
+/* 
+
+3. Escreva um programa que receba uma lista de números e retorne uma nova lista contendo apenas os números pares.
+4. Escreva um programa que receba uma lista de números e retorne a soma de todos os números.
+5. Escreva um programa que receba uma lista de palavras e retorne uma nova lista contendo apenas as palavras com mais de 5 caracteres. 
+*/
