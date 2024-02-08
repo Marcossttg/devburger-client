e6e18bd491b89b072031e94995158986b3a1eb9a
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-02-07 21:12:34 -0300

    exercise 3 finished

--- _JavaScript/js-modulo-04/Exercicios-04/script.js
+++ _JavaScript/js-modulo-04/Exercicios-04/script.js
@@ -11,14 +11,26 @@ console.log(n2);
 */
 
 // 2. Escreva um programa que receba uma lista de nomes e retorne uma nova lista com o comprimento de cada nome.
+/* 
 const nomes = ["Marcos", "Luiza", "Pedro", "Juliana", "Cesar"];
 const nomesComp = nomes.map(function (nome) {
     return `O nome ${nome} tem ${nome.length} letras:`;
 })
 console.log(nomesComp);
+*/
+
+//3. Escreva um programa que receba uma lista de números e retorne uma nova lista contendo apenas os números pares.
+
+let n = [1, 2, 3, 4, 5, 6, 7, 8];
+let nPar = n.filter(function (par) {
+    return par % 2 === 0;
+})
+    .map(function (par) {
+        return `Os Números pares são ${par}`;
+    })
+console.log(nPar);
 
 /* 
-3. Escreva um programa que receba uma lista de números e retorne uma nova lista contendo apenas os números pares.
 4. Escreva um programa que receba uma lista de números e retorne a soma de todos os números.
 5. Escreva um programa que receba uma lista de palavras e retorne uma nova lista contendo apenas as palavras com mais de 5 caracteres. 
 */
