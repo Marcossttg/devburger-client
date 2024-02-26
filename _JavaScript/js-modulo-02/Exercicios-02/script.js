05c89a4a0c78d3b3fd3f81397bc48992be1e0ece
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-02-25 23:36:30 -0300

    exercise

--- _JavaScript/js-modulo-02/Exercicios-02/script.js
+++ _JavaScript/js-modulo-02/Exercicios-02/script.js
@@ -1541,11 +1541,42 @@ let x = 40;
 let y = 10;
 let pct = clacPct(x, y);
 console.log( `${pct} % de ${x} é ${y}`);
-*/
+
 function clacPct(x, y) {
     return (y / x) * 100;
 }
 let x = 40;
 let y = 10;
 let pct = clacPct(x, y);
-console.log(`${pct} % de ${x} é ${y}`);
+console.log(`${pct}% de ${x} é ${y}`);
+*/
+
+/* 
+Calcule o preço do imóvel
+-m2 = 3.000
+-Se tiver 1 quarto, o m2 é 1x
+-Se tiver 2 quartos, o m2 é 1.2x
+-Se tiver 3 quartos, o m3 é 1.5x
+Uso da função:
+*/
+let metragem = 123;
+let quartos = 3;
+
+function calcularImovel(metragem, quartos) {
+    let m2 = 3000;
+    let preco = 0;
+    switch (quartos) {
+        case 1:
+            preco = metragem * m2;
+            break;
+        case 2:
+            preco = metragem * (m2 * 1.2);
+            break;
+        case 3:
+            preco = metragem * (m2 * 1.5);
+            break;
+    }
+    return preco;
+}
+let preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$ ${preco}`);
