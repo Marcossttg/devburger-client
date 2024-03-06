999adac21fab0d9f15286b4f538dcba6c92f381d
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-05 23:08:53 -0300

    creating user array

--- _React/codeClub-projects/my-app/src/App.js
+++ _React/codeClub-projects/my-app/src/App.js
@@ -1,16 +1,22 @@
 import React from "react"
 import ImgPeople from './assets/image-bg_people.svg';
 import ArrowRight from './assets/arrow-right.svg';
-
+import ImgLixeira from './assets/img-lixeira.svg';
 
 import {
   Container, Image,
   ContainerItens,
   H1, InputLabel,
   Input, Button,
+  User,
 } from "./styles"
 
 const App = () => {
+
+  const users = [
+    { id: Math.random(), name: "Pedro", age: 29 },
+    { id: Math.random(), name: "Maria", age: 20 },
+  ]
 
   return (
     <Container >
@@ -22,6 +28,17 @@ const App = () => {
         <InputLabel>Idade:</InputLabel>
         <Input placeholder="Digite sua Idade!"></Input>
         <Button>Cadastrar<img alt="arrow" src={ArrowRight} /> </Button>
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.age}</p>
+              <button>
+                <img src={ImgLixeira} />
+              </button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
     </Container>
   );
--- _React/codeClub-projects/my-app/src/styles.js
+++ _React/codeClub-projects/my-app/src/styles.js
@@ -3,7 +3,7 @@ import BgImgDisplay from './assets/bg-image_display.svg';
 import BgImgDisplay01 from './assets/bg-image_display-01.svg';
 import ImgPeople01 from './assets/image-bg_people-01.svg';
 import ArrowLeft from './assets/arrow-left.svg';
-import ImgLixeira from './assets/img-lixeira.svg';
+
 
 export const Container = styled.div`
 height: 100vh;
@@ -22,10 +22,10 @@ margin-top: 30px;
 
 export const ContainerItens = styled.div`
 background: linear-gradient(
-    157.44deg, 
-    rgba(255, 255, 255, 0.6)0.84%, 
-    rgba(255, 255, 255,0.6) 0.85%,
-    rgba(255, 255, 255, 0.15)100%);
+157.44deg, 
+rgba(255, 255, 255, 0.6)0.84%, 
+rgba(255, 255, 255,0.6) 0.85%,
+rgba(255, 255, 255, 0.15)100%);
 border-radius: 61px 61px 0px 0px;
 padding: 50px 30px;
 display: flex;
@@ -53,43 +53,72 @@ line-height: 22px;
 `;
 
 export const Input = styled.input`
-    width: 342px;
-    height: 58px;
-    background: rgba(255, 255, 255, 0.25);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 14px;
-    border: none;
-    outline: none;
-    padding-left: 25px;
-    color: #FFFFFF;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 28px;
-    margin-bottom: 25px;
+width: 342px;
+height: 58px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+border: none;
+outline: none;
+padding-left: 25px;
+color: #FFFFFF;
+font-style: normal;
+font-weight: normal;
+font-size: 20px;
+line-height: 28px;
+margin-bottom: 25px;
 
 `;
 
 export const Button = styled.button`
-    width: 342px;
-    height: 74px;
-    background: rgba(0, 0, 0, 0.8);
-    color: #FFFFFF;
-    border-radius: 14px;
-    border: none;
-    font-size: 17px;
-    font-style: normal;
-    font-weight:bold;
-    line-height: 28px;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 20px;
-    &:hover{
-        opacity: 0.8;
-    }
-    &:active{
-        opacity: 0.5;
-    }
+width: 342px;
+height: 74px;
+background: rgba(0, 0, 0, 0.8);
+color: #FFFFFF;
+border-radius: 14px;
+border: none;
+font-size: 17px;
+font-style: normal;
+font-weight:bold;
+line-height: 28px;
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+gap: 20px;
+&:hover{
+    opacity: 0.8;
+}
+&:active{
+    opacity: 0.5;
+}
 `;
+
+export const User = styled.li`
+display: flex;
+justify-content: space-around;
+align-items: center;
+width: 342px;
+height: 58px;
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+border: none;
+outline: none;
+margin-top: 25px;
+
+p {
+color: #FFFFFF;
+font-style: normal;
+font-weight: normal;
+font-size: 20px;
+line-height: 28px;
+}
+
+button {
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+`;
+
