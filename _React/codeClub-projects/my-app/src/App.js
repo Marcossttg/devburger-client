f74865c289768ffdaa7013784fa80adafc2149ed
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-06 20:39:41 -0300

    finished the first part

--- _React/codeClub-projects/my-app/src/App.js
+++ _React/codeClub-projects/my-app/src/App.js
@@ -1,4 +1,6 @@
-import React from "react"
+//Add react hooks ou ferramentas auxiliares
+import React, { useState } from "react";
+
 import ImgPeople from './assets/image-bg_people.svg';
 import ArrowRight from './assets/arrow-right.svg';
 import ImgLixeira from './assets/img-lixeira.svg';
@@ -13,27 +15,54 @@ import {
 
 const App = () => {
 
-  const users = [
-    { id: Math.random(), name: "Pedro", age: 29 },
-    { id: Math.random(), name: "Maria", age: 20 },
-  ]
+  // const users = [];
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+
+  //add novo user c/ React Hooks
+  function addNewUser() {
+    setUsers([...users, { id: Math.random(), name, age }]);
+  }
+  //pega evento de change no input
+  function changeInputName(event) {
+    setName(event.target.value);
+  }
+
+  //pega evento de change no input
+  function changeInputAge(event) {
+    setAge(event.target.value);
+  }
+
+  //deleta user utilizando filter
+  function deleteUser(userId) {
+    const newUsers = users.filter(user => user.id !== userId);
+    setUsers(newUsers);
+  }
 
   return (
     <Container >
       <Image alt="logo-imagem" src={ImgPeople} />
+
       <ContainerItens>
         <H1>Olá!</H1>
+
         <InputLabel>Nome:</InputLabel>
-        <Input placeholder="Digite seu nome!"></Input>
+        <Input onChange={changeInputName} placeholder="Digite seu nome!" />
+
         <InputLabel>Idade:</InputLabel>
-        <Input placeholder="Digite sua Idade!"></Input>
-        <Button>Cadastrar<img alt="arrow" src={ArrowRight} /> </Button>
+        <Input onChange={changeInputAge} placeholder="Digite sua Idade!" />
+
+        <Button onClick={addNewUser}>
+          Cadastrar<img alt="arrow" src={ArrowRight} />
+        </Button>
+
         <ul>
           {users.map((user) => (
             <User key={user.id}>
               <p>{user.name}</p>
               <p>{user.age}</p>
-              <button>
+              <button onClick={() => deleteUser(user.id)}>
                 <img src={ImgLixeira} />
               </button>
             </User>
