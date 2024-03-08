545703655855f5dec2e8441e743359c5108fcc12
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-08 19:48:24 -0300

    creation globalStyles

--- _React/codeClub-projects/my-app/src/index.js
+++ _React/codeClub-projects/my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/Home';
 import Users from './pages/Users';
-import GlobalStyle from './styles/resetStyle';
+import GlobalStyle from './styles/globalStyle';
 
 ReactDOM.render(
     <>
