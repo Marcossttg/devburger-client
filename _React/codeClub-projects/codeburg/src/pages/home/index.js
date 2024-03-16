ca59b95ddd4eae71c30d94176ba913af16727650
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-16 13:07:22 -0300

    created function to get the input value, using the useRef() hooks

--- _React/codeClub-projects/codeburg/src/pages/home/index.js
+++ _React/codeClub-projects/codeburg/src/pages/home/index.js
@@ -1,29 +1,76 @@
-import React from 'react';
+
+import React, { useState, useRef } from 'react';
 import ImgBurg from '../../assets/img/burger-1.svg';
 
 import { H2 } from '../../components/Title/styles';
 import { Button } from '../../components/Button/styles';
+
+
+//TIRAR 
+import ImgLixeira from '../../assets/img/img-lixeira.svg';
+//ate aki-----
 
 import {
   Image,
   Container,
   ContainerItens,
   P,
-  Input
+  Input,
+
+  /* tirar----- */
+  OrderList,
+  ParagraphOrder,
+  /* ate aki--- */
+
 } from './styles'
 
 const App = () => {
+
+  //add react hooks {useState}
+  const [requests, setRequests] = useState([]);
+  const InputOrder = useRef()
+  const InputName = useRef()
+
+  function addNewRequest() {
+    setRequests([...requests, {
+      id: Math.random(),
+      name: InputOrder.current.value,
+      order: InputName.current.value,
+    }]);
+    console.log(requests)
+  }
+
 
   return (
     <Container>
       <Image alt="logo da hamburgueria" src={ImgBurg} />
       <ContainerItens>
         <H2>Faça seu pedido!</H2>
+
         <P>Pedido</P>
-        <Input placeholder='Digite seu Pedido' />
+        <Input ref={InputOrder} placeholder='Digite seu Pedido' />
+
         <P>Nome do Cliente</P>
-        <Input placeholder='Digite seu Nome' />
-        <Button><P>Novo Pedido</P></Button>
+        <Input ref={InputName} placeholder='Digite seu Nome' />
+
+        <Button onClick={addNewRequest} >
+          <P>Novo Pedido</P>
+        </Button>
+        {/* TIRAR ESTA PARTE----- */}
+        <ul>
+          {requests.map((request) => (
+            <OrderList key={request.id}>
+              <div>
+                <ParagraphOrder>{request.order}</ParagraphOrder>
+                <P>{request.name}</P>
+              </div>
+              <button><img alt='imagem de uma lixeira' src={ImgLixeira} /></button>
+            </OrderList>
+          ))}
+        </ul>
+        {/* ate aki-------- */}
+
+
       </ContainerItens>
     </Container>
   );
--- _React/codeClub-projects/codeburg/src/pages/requests/styles.js
+++ _React/codeClub-projects/codeburg/src/pages/requests/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 background-color: #0A0A10;
-/* height: 100vh; */
 height: 100%;
 display: flex;
 flex-direction: column;
