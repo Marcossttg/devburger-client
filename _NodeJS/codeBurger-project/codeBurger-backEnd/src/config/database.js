f9779d62383d48e5e8c1e6c9e9eb9840fcd814ca
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-23 19:42:45 -0300

    created app/config/database folders. and sequelize file.

---
+++ _NodeJS/codeBurger-project/codeBurger-backEnd/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  passoword: 'dev',
+  database: 'codeburger',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
