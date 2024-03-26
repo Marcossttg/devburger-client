2e3ec9e2e1e16b151049feb7019ab17be0161475
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-25 22:08:55 -0300

    creating user model and configuring user modem

--- _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
+++ _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello Node' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Marcos outro',
+    email: 'marcos@emaiddl.com',
+    password_hash: '23advddbe11',
+  })
+  return response.json(user)
 })
 
 export default routes
