610680212aa98e45bcbe6f9eebef24f8c3d7515c
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-29 23:17:55 -0300

    creating model product. and finished codeburger node pt.1

--- _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
+++ _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
 
-import UserController from './app/controllers/UserController'
+import multer from 'multer'
+
+import multerConfig from './config/multer'
+
+import ProductController from './app/controllers/ProductController'
 
 import SessionController from './app/controllers/SessionController'
 
-import ProductController from './app/controllers/ProductController'
+import UserController from './app/controllers/UserController'
+
+const upload = multer(multerConfig)
 
 const routes = new Router()
 
@@ -12,6 +18,8 @@ routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
 
-routes.post('/products', ProductController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
+
+routes.get('/products', ProductController.index)
 
 export default routes
