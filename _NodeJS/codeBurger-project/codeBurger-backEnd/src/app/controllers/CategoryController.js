7e2e94d270af31c4bf58f627a9f8df48e9db79ed
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-31 20:33:17 -0300

    creating repeated category validation

--- _NodeJS/codeBurger-project/codeBurger-backEnd/src/app/controllers/CategoryController.js
+++ _NodeJS/codeBurger-project/codeBurger-backEnd/src/app/controllers/CategoryController.js
@@ -3,22 +3,34 @@ import Category from '../models/Category'
 
 class CategoryController {
   async store(request, response) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required(),
-    })
-
     try {
-      await schema.validateSync(request.body, { abortEarly: false })
+      const schema = Yup.object().shape({
+        name: Yup.string().required(),
+      })
+
+      try {
+        await schema.validateSync(request.body, { abortEarly: false })
+      } catch (err) {
+        return response.status(400).json({ error: err.errors })
+      }
+
+      const { name } = request.body
+
+      const categoryExists = await Category.findOne({
+        where: { name },
+      })
+
+      if (categoryExists) {
+        return response.status(400).json({ error: 'Category already exists' })
+      }
+
+      const { id } = await Category.create({
+        name,
+      })
+      return response.json({ name, id })
     } catch (err) {
-      return response.status(400).json({ error: err.errors })
+      console.log(err)
     }
-
-    const { name } = request.body
-
-    const category = await Category.create({
-      name,
-    })
-    return response.json(category)
   }
 
   async index(request, response) {
