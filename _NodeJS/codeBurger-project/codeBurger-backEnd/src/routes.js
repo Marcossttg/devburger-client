20183d8099a99a80306da85fa57c9cad222ad93f
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-03-30 22:24:50 -0300

    creating category table

--- _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
+++ _NodeJS/codeBurger-project/codeBurger-backEnd/src/routes.js
@@ -5,10 +5,9 @@ import multer from 'multer'
 import multerConfig from './config/multer'
 
 import ProductController from './app/controllers/ProductController'
-
 import SessionController from './app/controllers/SessionController'
-
 import UserController from './app/controllers/UserController'
+import CategoryController from './app/controllers/CategoryController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -27,5 +26,9 @@ routes.use(authMiddleware)
 routes.post('/products', upload.single('file'), ProductController.store)
 
 routes.get('/products', ProductController.index)
+
+routes.post('/categories', CategoryController.store)
+
+routes.get('/categories', CategoryController.index)
 
 export default routes
