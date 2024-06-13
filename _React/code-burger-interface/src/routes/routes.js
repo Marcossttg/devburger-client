d68784eeab4cf09fb2033b8b1365b9afe34349bb
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-06-12 22:02:36 -0300

    created private routes

--- _React/code-burger-interface/src/routes/routes.js
+++ _React/code-burger-interface/src/routes/routes.js
@@ -4,6 +4,8 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import Login from "../containers/Login"
 import Register from "../containers/Register";
+import Home from "../containers/Home";
+import PrivateRoute from "./private-route";
 
 function Routes() {
 
@@ -12,6 +14,7 @@ function Routes() {
 			<Switch>
 				<Route component={Login} path="/login"></Route>
 				<Route component={Register} path="/cadastro"></Route>
+				<PrivateRoute exact component={Home} path="/"></PrivateRoute>
 			</Switch>
 		</Router>
 	)
