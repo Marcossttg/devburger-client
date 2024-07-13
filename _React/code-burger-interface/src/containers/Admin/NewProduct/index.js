07ff0174553b211aca5edeee3974e23d46ffbcc9
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-07-13 12:13:05 -0300

    creating product addition form

--- _React/code-burger-interface/src/containers/Admin/NewProduct/index.js
+++ _React/code-burger-interface/src/containers/Admin/NewProduct/index.js
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form"
 
-import { Container } from "./styles";
+import { Container, Label, Input } from "./styles";
 import api from "../../../services/api";
+import ReactSelect from "react-select";
+import { Button } from "../../../components"
 
 
 function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
 
   useEffect(() => {
     async function loadOrders() {
@@ -16,7 +22,19 @@ function NewProduct() {
 
   return (
     <Container>
-      <div>Olá</div>
+      <form noValidate>
+        <Label>Nome</Label>
+        <Input type="text" {...register("name")} />
+
+        <Label>Preço</Label>
+        <Input type="number" {...register("price")} />
+
+        <Label>Upload da imagem</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+        <Button>Adicionar produto</Button>
+      </form>
     </Container>
   );
 }
--- _React/code-burger-interface/src/containers/Admin/NewProduct/styles.js
+++ _React/code-burger-interface/src/containers/Admin/NewProduct/styles.js
@@ -4,3 +4,11 @@ export const Container = styled.div`
 
 `;
 
+export const Label = styled.p`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
