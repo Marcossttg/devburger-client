d3bd628cac94d184bbd0edd4b79368e36ba2f6f5
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-08-15 23:30:42 -0300

    Error: The command yarn run build exited with 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import pluginJs from '@eslint/js'
+import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+]
