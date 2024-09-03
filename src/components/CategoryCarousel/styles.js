61ea490b9f889fa8e8a55936887bcb8caf8bd001
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-03 16:06:15 -0300

    add height image

--- src/components/CategoryCarousel/styles.js
+++ src/components/CategoryCarousel/styles.js
@@ -37,6 +37,7 @@ export const ContainerItems = styled.div`
 
 export const Image = styled.img`
   width: 200px;
+  height: 200px;
   border-radius: 10px;
 `
 
--- src/components/OffersCarousel/styles.js
+++ src/components/OffersCarousel/styles.js
@@ -45,6 +45,7 @@ export const ContainerItems = styled.div`
 
 export const Image = styled.img`
   width: 200px;
+  height: 200px;
   border-radius: 10px;
   margin-bottom: 16px;
 `
